939ef538c4908c0256ebef63f19c3e15e19648d9
Mamba <dhm281989338@hotmail.com>
2020-04-28 01:11:15 +0800

    配置项目所需的UI组件库antd

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+// 按需引入antd组件标签
+import {Button} from 'antd'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        App...
+        <h2>App...</h2>
+        <Button>chilk me</Button>
+        <Button type="primary">touch me</Button>
       </div>
     )
   }
